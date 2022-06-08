3edcae37ae8609d4de19b9c0f3840791ab6b8c56
Carl Parker <carljparker@fb.com>
2022-06-07 21:11:20 -0700

    Add these tests . . . again as a starting point for the new project

---
+++ tests/test_main.py
@@ -0,0 +1,511 @@
+'''
+Unit test module for functions in main.py
+'''
+
+# pylint: disable=C0305
+# pylint: disable=R0904
+
+from collections.abc import Iterable
+
+import unittest
+
+import socialnetwork_model as sn
+
+import main
+
+
+class TestMain(unittest.TestCase):
+    '''
+    Class definition for unit tests for main.py
+    '''
+    def test_init_user_collection(self):
+        '''
+        Test initialization of the collection of users
+        '''
+        user_col = main.init_user_collection()
+        self.assertIsInstance(
+            user_col,
+            sn.UserCollection,
+            msg="Returned object not UserCollection"
+        )
+
+    def test_init_status_collection(self):
+        '''
+        Test initialization of the collection of statuses
+        '''
+        status_col = main.init_status_collection()
+        self.assertIsInstance(
+            status_col,
+            sn.UserStatusCollection,
+            msg="Returned object not UserStatusCollection"
+        )
+
+    def test_1_load_users(self):
+        '''
+        Test loading users from a CSV into the collection
+        '''
+        user_col = main.init_user_collection()
+        self.assertIs( main.load_users( "accounts.csv", user_col ), True )
+
+    def test_2_load_status_updates(self):
+        '''
+        Test loading statuses from a CSV into the collection
+        '''
+        status_col = main.init_status_collection()
+        self.assertIs(
+            main.load_status_updates(
+                "status_updates_reasonable.csv",
+                status_col
+            ),
+            True
+        )
+
+    def test_save_users(self):
+        '''
+        Test saving users from the collection to a CSV
+        '''
+        user_col = main.init_user_collection()
+        main.load_users( "accounts.csv", user_col )
+        self.assertIs( main.save_users( "accounts-new.csv", user_col), True )
+
+    def test_load_bad_users(self):
+        '''
+        Test how we handle loading a malformed CSV of users
+        '''
+        user_col = main.init_user_collection()
+        self.assertIs( main.load_users( "accounts-bad.csv", user_col ), False)
+
+    @unittest.skip( "Perf testing only" )
+    def test_10k_load_status_updates(self):
+        '''
+        Test loading statuses from a CSV into the collection
+        '''
+        status_col = main.init_status_collection()
+        self.assertIs(
+            main.load_status_updates(
+                "status_updates_10K.csv",
+                status_col
+            ),
+            True
+        )
+
+    @unittest.skip( "Perf testing only" )
+    def test_5k_load_status_updates(self):
+        '''
+        Test loading statuses from a CSV into the collection
+        '''
+        status_col = main.init_status_collection()
+        self.assertIs(
+            main.load_status_updates(
+                "status_updates_5K.csv",
+                status_col
+            ),
+            True
+        )
+
+    def test_2k_load_status_updates(self):
+        '''
+        Test loading statuses from a CSV into the collection
+        '''
+        user_col = main.init_user_collection()
+        self.assertIs( main.load_users( "accounts.csv", user_col ), True )
+        status_col = main.init_status_collection()
+        self.assertIs(
+            main.load_status_updates(
+                "status_updates_2K.csv",
+                status_col
+            ),
+            True
+        )
+
+    @unittest.skip( "Perf testing only" )
+    def test_1k_load_status_updates(self):
+        '''
+        Test loading statuses from a CSV into the collection
+        '''
+        status_col = main.init_status_collection()
+        self.assertIs(
+            main.load_status_updates(
+                "status_updates_1K.csv",
+                status_col
+            ),
+            True
+        )
+    def test_load_bad_status_updates(self):
+        '''
+        Test how we handle loading a malformed CSV of statuses
+        '''
+        status_col = main.init_status_collection()
+        self.assertIs(
+            main.load_status_updates(
+                "status_updates_bad.csv",
+                status_col
+            ),
+            False
+        )
+
+    def test_add_user(self):
+        '''
+        Test whether we can add a user
+        '''
+        user_col = main.init_user_collection()
+        self.assertIs( main.load_users( "accounts.csv", user_col ), True )
+        self.assertIs(
+            main.add_user(
+                "cajopa",
+                "carljparker@gmail.com",
+                "Carl",
+                "Parker",
+                user_col
+            ),
+            True
+        )
+
+    def test_add_user_fail(self):
+        '''
+        Test how we handle trying to add a malformed user
+        '''
+        user_col = main.init_user_collection()
+        self.assertIs( main.load_users( "accounts.csv", user_col ), True )
+        self.assertIs(
+            main.add_user(
+                "evmiles97",
+                "eve.miles@uw.edu",
+                "Eve",
+                "Miles",
+                user_col
+            ),
+            True
+        )
+        #
+        # Add same user a second time
+        #
+        self.assertIsNot(
+            main.add_user(
+                "evmiles97",
+                "eve.miles@uw.edu",
+                "Eve",
+                "Miles",
+                user_col
+            ),
+            True
+        )
+
+    def test_update_user(self):
+        '''
+        Test whether we can update the data for a user
+        '''
+        user_col = main.init_user_collection()
+        self.assertIs( main.load_users( "accounts.csv", user_col ), True )
+        self.assertIs(
+            #
+            # Ms Miles got married and changed her last name
+            #
+            main.update_user(
+                "evmiles97",
+                "eve.miles@uw.edu",
+                "Eve",
+                "Kilos",
+                user_col
+            ),
+            True
+        )
+        return True
+
+    def test_update_user_fail(self):
+        '''
+        Test whether the code to add a user fails appropriately
+        '''
+        user_col = main.init_user_collection()
+        self.assertIs( main.load_users( "accounts.csv", user_col ), True )
+        self.assertIsNot(
+            #
+            # Ms Miles got married and changed her last name
+            #
+            main.update_user(
+                "evmiles9x",
+                "eve.miles@uw.edu",
+                "Eve",
+                "Kilos",
+                user_col
+            ),
+            True
+        )
+        return True
+
+    def test_delete_user(self):
+        '''
+        Test whether we can delete a user from the collection
+        '''
+        user_col = main.init_user_collection()
+        main.load_users( "accounts.csv", user_col )
+        test_user_id = 'Isabel.Avivah34'
+        self.assertIs( main.delete_user( test_user_id, user_col ), True )
+        self.assertIs(
+            main.save_users(
+                "accounts-with-delete.csv",
+                user_col
+            ),
+            True
+        )
+
+    def test_delete_user_fail(self):
+        '''
+        Test whether the code to delete a user fails when we expect it to
+        '''
+        user_col = main.init_user_collection()
+        main.load_users( "accounts.csv", user_col )
+        test_user_id = 'dave0x'
+        self.assertIsNot( main.delete_user( test_user_id, user_col ), True )
+        self.assertIs(
+            main.save_users(
+                "accounts-with-delete.csv",
+                user_col
+            ),
+            True
+        )
+
+    def test_search_user(self):
+        '''
+        Test whether we can find a user that we know exists
+        '''
+        user_col = main.init_user_collection()
+        main.load_users( "accounts.csv", user_col )
+        test_user_id = 'dave03'
+        self.assertIsInstance(
+            main.search_user( test_user_id, user_col ),
+            sn.UsersTable,
+            msg = "Failed to find user"
+        )
+
+    def test_search_user_fail(self):
+        '''
+        Test whether we fail to find a user that we know does *not* exist
+        '''
+        user_col = main.init_user_collection()
+        main.load_users( "accounts.csv", user_col )
+        test_user_id = 'dave0x'
+        self.assertNotIsInstance(
+            main.search_user( test_user_id, user_col ),
+            sn.UsersTable,
+            msg = "Failed to find user"
+        )
+
+    def test_add_status(self):
+        '''
+        Test whether we can add a status to the collection
+        '''
+        status_col = main.init_status_collection()
+        self.assertIs(
+            main.load_status_updates(
+                "status_updates_reasonable.csv",
+                status_col
+            ),
+            True
+        )
+        user_col = main.init_user_collection()
+        self.assertIs( main.load_users( "accounts.csv", user_col ), True )
+        self.assertIs( main.add_user( "pfram", "pfram@yahoo.com", "Peter", "Frampton", user_col ), True )
+        self.assertIs(
+            main.add_status(
+                "pfram",
+                "pfram_777",
+                "I can has status",
+                status_col
+            ),
+            True
+        )
+
+    def test_add_status_fail(self):
+        '''
+        Test whether we fail to add a status when we expect to fail
+        '''
+        user_col = main.init_user_collection()
+        self.assertIs( main.load_users( "accounts.csv", user_col ), True )
+        status_col = main.init_status_collection()
+        self.assertIs(
+            main.load_status_updates(
+                "status_updates_reasonable.csv",
+                status_col
+            ),
+            True
+        )
+        self.assertIsNot(
+            main.add_status(
+                "evmiles97",
+                "evmiles97_00002",
+                "Perfect weather for a hiking",
+                status_col
+            ),
+            True
+        )
+
+    def test_update_status(self):
+        '''
+        Test whether we can update the data for a status
+        '''
+        user_col = main.init_user_collection()
+        self.assertTrue( main.load_users( "accounts.csv", user_col ) )
+        status_col = main.init_status_collection()
+        self.assertIs(
+            main.load_status_updates(
+                "status_updates_reasonable.csv",
+                status_col
+            ),
+            True
+        )  
+        self.assertIs(
+            main.update_status(
+                "Isabel.Avivah34_27",
+                "Isabel.Avivah34",
+                "Perfect weather for a skiing",
+                status_col
+            ),
+            True
+        )
+        self.assertIs(
+            main.save_status_updates(
+                "status_updates-with-mod.csv",
+                status_col
+            ),
+            True
+        )
+
+    def test_update_status_fail(self):
+        '''
+        Test whether trying to update a status fails when we expect it to
+        '''
+        status_col = main.init_status_collection()
+        self.assertIs(
+            main.load_status_updates(
+                "status_updates_reasonable.csv",
+                status_col
+            ),
+            True
+        )
+        self.assertIsNot(
+            main.update_status(
+                "cajopa_00001",
+                "cajopa",
+                "I can *have* status",
+                status_col
+            ),
+            True
+        )
+
+    def test_delete_status(self):
+        '''
+        Test whether we can delete a status from the collection
+        '''
+        status_col = main.init_status_collection()
+        main.load_status_updates( "status_updates_reasonable.csv", status_col )
+        test_status_id = 'Isabel.Avivah34_27'
+        self.assertIs(
+            main.delete_status(
+                test_status_id,
+                status_col ),
+            True
+        )
+        self.assertIs(
+            main.save_status_updates(
+                "status_updates-with-delete.csv",
+                status_col
+            ),
+            True
+        )
+
+    def test_delete_status_fail(self):
+        '''
+        Test whether we fail to delete a status when we expect it to fail
+        '''
+        status_col = main.init_status_collection()
+        main.load_status_updates( "status_updates_reasonable.csv", status_col )
+        test_status_id = 'evmiles97_0000x'
+        self.assertIsNot(
+            main.delete_status(
+                test_status_id, status_col
+            ),
+            True
+        )
+        self.assertIs(
+            main.save_status_updates(
+                "status_updates-with-delete.csv",
+                status_col
+            ),
+            True
+        )
+
+    def test_search_status(self):
+        '''
+        Test whether we can find a status that we know is there
+        '''
+        user_col = main.init_user_collection()
+        self.assertTrue( main.load_users( "accounts.csv", user_col ) )
+        status_col = main.init_status_collection()
+        status_data = "status_updates_reasonable.csv"
+        test_status_id = 'Isabel.Avivah34_27'
+        self.assertTrue( main.load_status_updates( status_data, status_col ) )
+        self.assertIsInstance(
+            main.search_status( test_status_id, status_col ),
+            sn.StatusTable,
+            msg = "Failed to find status [" + 
+                  test_status_id + 
+                  "] in [" + 
+                  status_data +
+                  "]" 
+        )
+
+    def test_search_all_status_updates(self):
+        '''
+        Test whether we can find all the status updates for a 
+        user that we know has multiple updates.
+        '''
+        user_col = main.init_user_collection()
+        self.assertTrue( main.load_users( "accounts.csv", user_col ) )
+        status_col = main.init_status_collection()
+        status_data = "status_updates_reasonable.csv"
+        self.assertTrue( main.load_status_updates( status_data, status_col ) )
+        test_user_id = 'Tonye.Nella57'
+        list_of_status = main.search_all_status_updates( test_user_id,
+                                                         status_col )
+        self.assertIsInstance(
+            list_of_status,
+            list,
+            msg = "main.search_all_status_updates() did not return a list" )
+
+    def test_filter_status_by_string(self):
+        '''
+        Test whether we can find all the status updates for a 
+        user that we know has multiple updates.
+        '''
+        user_col = main.init_user_collection()
+        self.assertTrue( main.load_users( "accounts.csv", user_col ) )
+        status_col = main.init_status_collection()
+        status_data = "status_updates_reasonable.csv"
+        self.assertTrue( main.load_status_updates( status_data, status_col ) )
+        test_target_text = 'zebra'
+        status_iterator = main.filter_status_by_string( test_target_text,
+                                                        status_col )
+        self.assertIsInstance(
+            status_iterator,
+            Iterable,
+            msg = "main.filter_status_by_string did not return an iterator" )
+        self.assertEquals( len( [ stat for stat in status_iterator ] ), 3 )
+
+
+    def test_search_status_fail(self):
+        '''
+        Test whether we fail to find a status that we know is not there
+        '''
+        status_col = main.init_status_collection()
+        main.load_status_updates( "status_updates_reasonable.csv", status_col )
+        test_status_id = 'evmiles97_0000x'
+        self.assertNotIsInstance(
+            main.search_status( test_status_id, status_col ),
+            sn.StatusTable,
+            msg = "Failed to find status"
+        )
+
+
+# --- END --- #
+
---
+++ tests/test_sn.py
@@ -0,0 +1,146 @@
+'''
+Unit test module for functions in main.py
+'''
+
+# pylint: disable=C0305
+# pylint: disable=R0904
+
+
+import unittest
+
+import socialnetwork_model as sn
+
+from collections.abc import Iterable
+
+
+class TestSN(unittest.TestCase):
+    '''
+    Class definition for unit tests for main.py
+    '''
+
+    def setUp(self):
+        '''
+        Test initialization of the users and status collections
+        '''
+        self.user_col = sn.UserCollection()
+        self.assertIsInstance(
+            self.user_col,
+            sn.UserCollection,
+            msg="Returned object not user_collection"
+        )
+
+        self.status_col = sn.UserStatusCollection()
+        self.assertIsInstance(
+            self.status_col,
+            sn.UserStatusCollection,
+            msg="Returned object not status_collection"
+        )
+
+    def test_user_cache( self ):
+        sn.UserCache().store( "cajopa" )
+        self.assertTrue( sn.UserCache().read( "cajopa" ) )
+
+        sn.UserCache().erase( "cajopa" )
+        self.assertFalse( sn.UserCache().read( "cajopa" ) )
+
+    def test_add_user(self):
+        new_user = self.user_col.add_user( "cajopa", "cajopa@uw.edu", "Carl", "Parker" )
+        self.assertTrue( new_user )
+
+    def test_search_user(self):
+        self.user_col.add_user( "lrac", "lrac@yahoo.edu", "Carl", "Parker" )
+        user = self.user_col.search_user( "lrac" )
+        self.assertEqual( user.user_id, "lrac" )
+
+    def test_search_user_not_found(self):
+        user = self.user_col.search_user( "carlp-20220509" )
+        self.assertNotIsInstance(
+            user,
+            #
+            # CHECKCHECK: Correct type below?
+            #
+            sn.UsersTable
+        )
+
+    def test_mod_user(self):
+        add_success = self.user_col.add_user( "spiderman", "peterpa@uw.edu", "Carl", "Parker" )
+        self.assertTrue( add_success )
+        mod_success = self.user_col.modify_user( "spiderman", "peterpa@uw.edu", "Peter", "Parker" ) 
+        self.assertTrue( mod_success )
+        find_mod_user = self.user_col.search_user( "spiderman" )
+        self.assertEqual( find_mod_user.user_name, "Peter" )
+
+    def test_delete_user(self):
+        add_success = self.user_col.add_user( "batman", "brucew@uw.edu", "Bruce", "Wayne" ) 
+        self.assertTrue( add_success )
+        del_success = self.user_col.delete_user( "batman" )
+        self.assertTrue( del_success )
+
+
+    def test_add_status(self):
+        self.user_col.add_user( "capra", "capra@uw.edu", "Eddie", "Bosco" )
+        new_status = self.status_col.add_status( "capra", "capra_0007", "HBO Max ads...:-P" )
+        self.assertTrue( new_status )
+
+    def test_search_status(self):
+        self.user_col.add_user( "bmeau", "bmeau@uw.edu", "Bart", "Muller" )
+        new_status = self.status_col.add_status( "bmeau", "bmeau_0008", "Netflix ads as well" )
+        self.assertTrue( new_status )
+        status = self.status_col.search_status( "bmeau_0008" )
+        self.assertEqual( status.status_text, "Netflix ads as well" )
+
+    def test_modify_status(self):
+        self.user_col.add_user( "pfram", "pfram@uw.edu", "Peter", "Frampton" )
+        new_status = self.status_col.add_status( "pfram", "pfram_0001", "I prefer Disney" )
+        self.assertTrue( new_status )
+        mod_success = self.status_col.modify_status( "pfram_0001", "pfram", "I heart Disney" )
+        self.assertTrue( mod_success )
+
+    def test_delete_status(self):
+        self.user_col.add_user( "jimmypage", "jimmyp@uw.edu", "Jimmy", "Page" )
+        new_status = self.status_col.add_status( "jimmypage", "jimmypage_0002", "BBC for me" )
+        self.assertTrue( new_status )
+        del_success = self.status_col.delete_status( "jimmypage_0002" )
+        self.assertTrue( del_success )
+        #
+        # See if we can find the status we deleted
+        #
+        status = self.status_col.search_status( "jimmypage_0002" )
+        self.assertIsNone( status )
+
+    def test_delete_status_cascade(self):
+        self.user_col.add_user( "rogerd", "rogerd@uw.edu", "Roger", "Daltry" )
+        new_status = self.status_col.add_status( "rogerd", "rogerd_0009", "Can you see the real me?" )
+        self.assertTrue( new_status )
+        del_success = self.user_col.delete_user( "rogerd" )
+        self.assertTrue( del_success )
+        #
+        # See if we can find the status we deleted.
+        #
+        # Should have been recursively deleted.
+        #
+        status = self.status_col.search_status( "rogerd_0009" )
+        self.assertIsNone( status )
+
+    def test_search_all_status_updates(self):
+        self.user_col.add_user( "maddrox", "maddrox@uw.edu", "Bart", "Muller" )
+        new_status = self.status_col.add_status( "maddrox", "maddrox_0008", "Netflix ads as well" )
+        self.assertTrue( new_status )
+        new_status = self.status_col.add_status( "maddrox", "maddrox_0009", "Amazon Prime has no ads" )
+        self.assertTrue( new_status )
+        status_list = self.status_col.search_all_status_updates( "maddrox" )
+        self.assertIsInstance( status_list, list )
+
+    def test_filter_status_by_string(self):
+        self.user_col.add_user( "lukec", "lukec@uw.edu", "Luke", "Cage" )
+        new_status = self.status_col.add_status( "lukec", "lukec_0008", "Netflix hosts my show" )
+        self.assertTrue( new_status )
+        new_status = self.status_col.add_status( "lukec", "lukec_0009", "Amazon Prime is too lame to host my show" )
+        self.assertTrue( new_status )
+        status_iterator = self.status_col.filter_status_by_string( "host" )
+        self.assertIsInstance( status_iterator, Iterable )
+        self.assertEqual( len( [ stat.status_text for stat in status_iterator ] ), 2 )
+
+
+# --- END --- #
+
