09180d8ec0de09d4e2a91afe323cff3499bffa35
Carl Parker <carljparker@fb.com>
2022-06-13 11:25:11 -0700

    Update these scripts to work in this installable environ

--- run-unittest-test_main.sh
+++ run-unittest-test_main.sh
@@ -1,5 +1,16 @@
+#
+# Run this script from the top-level directory. For example:
+#
+#     ~/git/github/Python320-ToDoApp
+#
 
-rm sn.db & python3 -m unittest -v test_main.TestMain
+#
+# Need to set the PYTHONPATH so that the tests can find and import the
+# modules in `src` to be tested.
+#
+export PYTHONPATH=$PYTHONPATH:~/git/github/Python320-ToDoApp/src
+
+rm sn.db & python3 -m unittest -v tests.test_main.TestMain
 
 
 # --- END --- #
