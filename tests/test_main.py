39a8e482dcafc1594f60204c27615fa2ae1eb910
Carl Parker <carljparker@fb.com>
2022-06-16 19:24:24 -0700

    Remove more user func and tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -40,20 +40,6 @@ class TestMain(unittest.TestCase):
             msg="Returned object not ToDoCollection"
         )
 
-    def test_1_load_users(self):
-        '''
-        Test loading users from a CSV into the collection
-        '''
-        user_col = main.init_user_collection()
-        self.assertIs( main.load_users( "accounts.csv", user_col ), True )
-
-    def test_load_bad_users(self):
-        '''
-        Test how we handle loading a malformed CSV of users
-        '''
-        user_col = main.init_user_collection()
-        self.assertIs( main.load_users( "accounts-bad.csv", user_col ), False)
-
     def test_2_load_todo_updates(self):
         '''
         Test loading todos from a CSV into the collection
@@ -81,79 +67,9 @@ class TestMain(unittest.TestCase):
             False
         )
 
-    def test_add_user(self):
-        '''
-        Test whether we can add a user
-        '''
-        user_col = main.init_user_collection()
-        self.assertIs( main.load_users( "accounts.csv", user_col ), True )
-        self.assertIs(
-            main.add_user(
-                "cajopa",
-                "carljparker@gmail.com",
-                "Carl",
-                "Parker",
-                user_col
-            ),
-            True
-        )
-
-    def test_add_user_fail(self):
-        '''
-        Test how we handle trying to add a malformed user
-        '''
-        user_col = main.init_user_collection()
-        self.assertIs( main.load_users( "accounts.csv", user_col ), True )
-        self.assertIs(
-            main.add_user(
-                "evmiles97",
-                "eve.miles@uw.edu",
-                "Eve",
-                "Miles",
-                user_col
-            ),
-            True
-        )
-        #
-        # Add same user a second time
-        #
-        self.assertIsNot(
-            main.add_user(
-                "evmiles97",
-                "eve.miles@uw.edu",
-                "Eve",
-                "Miles",
-                user_col
-            ),
-            True
-        )
 
 
-    def test_search_user(self):
-        '''
-        Test whether we can find a user that we know exists
-        '''
-        user_col = main.init_user_collection()
-        main.load_users( "accounts.csv", user_col )
-        test_user_id = 'dave03'
-        self.assertIsInstance(
-            main.search_user( test_user_id, user_col ),
-            sn.UsersTable,
-            msg = "Failed to find user"
-        )
 
-    def test_search_user_fail(self):
-        '''
-        Test whether we fail to find a user that we know does *not* exist
-        '''
-        user_col = main.init_user_collection()
-        main.load_users( "accounts.csv", user_col )
-        test_user_id = 'dave0x'
-        self.assertNotIsInstance(
-            main.search_user( test_user_id, user_col ),
-            sn.UsersTable,
-            msg = "Failed to find user"
-        )
 
     def test_add_todo(self):
         '''
